c1b689a7e320fbdb9df4c8062a983332e12b45f3
rade <bajic.rade2@gmail.com>
2019-12-29 11:25:35 +0100

    wwo hooks, one method, and model defined

---
+++ dist/resources/user/user.controllers.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @VAZNO
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * @OPCIJA
+ *
+ *
+ *
+ */
+/* OVO JE RESENJA A NE O NO DOLE
+
+save ZA CREATION
+
+
+update ZA UPDATE
+
+
+cultSchema.pre('save', function (next) {
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+    console.log(this.isModified('amount'))
+
+    // this.isModified
+
+    next()
+
+})
+
+cultSchema.pre('findOneAndUpdate', function (next) {
+    console.log('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+
+    console.log(this.getUpdate())
+
+    next()
+
+}) */
