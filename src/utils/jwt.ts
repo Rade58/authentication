acc10e82a748893ee8b3fec715b4f0a179bef20f
rade <bajic.rade2@gmail.com>
2019-12-30 04:29:43 +0100

    NOTING SOMETHING

---
+++ src/utils/jwt.ts
@@ -0,0 +1,7 @@
+import jwt from 'jsonwebtoken'
+
+// SECRET CE OBICNO BITI PROVIDED KROZ NEKI config FAJL, KAKAV SAM VIDEO U WORKSHOP-U
+// ALI ZA SADA SECRET CU UZIMATI DIREKTNO SA process-A
+
+
+// export const createToken = jwt.sign()
