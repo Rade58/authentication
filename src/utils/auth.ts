7a5d3cc11e0698dbb6e24bd72b8549124c088f50
rade <bajic.rade2@gmail.com>
2019-12-31 03:49:23 +0100

    signin controller

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -20,6 +20,9 @@ export const signUp: RequestHandler = async (req: Request, res: Response, next: 
 
     const token = createToken((user as any))
 
+    // OVAJ KORAK MI JE UPITAN, MOZDA SAM TREBAO POSLATI token KAO VREDNOST Authorization HEADER-A
+    // USTVARI CITAJUCI NEKE CLANKE VIDEO SAM
+
     res.status(201).send({ token })
 
   } catch (error) {
@@ -38,3 +41,42 @@ export const signUp: RequestHandler = async (req: Request, res: Response, next: 
 
 // DA ALI OVDE MORAJU NARAVNO DA SE OBAVE DODATNE PROVERE, KAO STO JE PROVERA PASSWORDA (AKO SE SECAS UPRAVO SI KREIRAO METODU NA USER SCHEMA-I
 //                                                                                        KOJA UPRAVO VALIDIRA SIFRU) 
+
+const signIn: RequestHandler = async (req: Request, res: Response) => {
+
+  const { email, password }: { email: string, password: string } = req.body
+
+  if (!email || !password) return res.status(400).send({ message: "Need email or password" })
+
+  const invalid = { message: 'Invalid email and password combination!' }
+
+  try {
+
+    const user = await User.findOne({ email }).select('email password').exec()      // AKO NE BUDE BIO PROVIDED U SELECTION DODAJ I checkPassword
+
+    if (!user) return res.status(401).send(invalid)
+
+    // IMAMO OVDE NESTED TRY CATCH BLOK, ZBOG TOGA KAKO IZGLEDA     checkPassword
+    // NJOJ JE Promise POVRATNA VREDNOST (I TO PROMISE KOJI MOZE REJECT-OVATI I LI RESOLVE-OVATI)
+
+    try {
+
+      await (user as any).checkPassword(password)
+
+      const token = createToken({ _id: user._id, email: (user as any).email })
+
+      return res.status(201).send({ token })
+
+    } catch (err) {
+      return res.status(401).send(invalid)
+    }
+
+
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).end()
+  }
+
+}
+
